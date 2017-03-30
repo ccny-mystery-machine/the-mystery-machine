83f6fa63d3ef607a30e1fe06e489db03003bc797
Gautam <ramasubg2@gmail.com>
2017-03-30 16:36:01 -0400

    Commenting and Selection Fixed Threshold

--- tree_search/main.py
+++ tree_search/main.py
@@ -19,11 +19,35 @@ def print_rollout():
     print( rollout_story(root_node, 10) )
 
 if __name__ == "__main__":
-    root_state = random_state()
+    
+    # Randomly assigns actors, places, and items for story
+    root_state = random_state(3,3) 
+    
+    # Initialize Root Node - Possible Methods boolean MUST BE TRUE 
     root_node = TreeNode(root_state, parent_edge=None, possible_methods=True)
+
+    # Total methods in story
     num_methods = len(root_node.possible_methods)
-    # max_numsim > num_methods * thres
-    n, s = mcts(root_node, max_iter=10, max_numsim=3000, max_simlength=20, C=1, thres=15) 
+    
+    """
+    The following 
+        max_numsim = max_expansion * thres
+
+    max_iter : Number of sentances in story = number of story nodes - 1 = number of story edges
+    max_expansion : Number of expansions in search
+    max_simlength : Maximum length of rollout
+    C : Exploration Constant for selection
+    thres : Minimum MCTS Visits for node expansion
+    """
+    # Perform Monte Carlo - returns final node and whole story
+    max_exp = 100
+    if max_exp < len(root_node.possible_methods):
+        raise ValueError("Max exp ({}) should be greater than num methods({})".format(max_exp, len(root_node.possible_methods)))
+
+
+    n, s = mcts(root_node, max_iter=10, max_expansion=100, max_simlength=20, C=1, thres=20) 
+    
+    # Print out results
     print(s)
     print(n.believability)
     print(n.value)
--- tree_search/state.py
+++ tree_search/state.py
@@ -13,27 +13,42 @@ class State:
         self.items = deepcopy(items)
 
 
-def random_state():
-    num_characters = randint(3, 5)
+def random_state(min_num_char, max_num_char):
+    
+    num_characters = randint(min_num_char, max_num_char)
+    
+    # Check for sanity
     if num_characters > len(NAME_BANK):
         raise IndexError("Number of character exceeds name bank")
+    
+    # Initialize state without actors or actor homes
     rand_state = State({}, OUT_PLACES, ITEMS)
+
+    # Keep track of actors already added to state
     seen_idx = []
+
+    # Adding an actor + actor home to the state
     for _ in range(num_characters):
+        
+        # Ensure unique actor names
         while True:
             ridx = randint(0,len(NAME_BANK)-1)
             if ridx not in seen_idx:
                 seen_idx.append(ridx)
                 break
+
+        # Setting up the keys for actor and place dictionaries
         name, gender = NAME_BANK[ridx]
         up_name = name.upper()
         up_place = up_name + "S_HOUSE"
 
+        # Add in the actor's house to the state
         rand_state.places[up_place] = deepcopy(PLACE_TEMPLATE)
         place = rand_state.places[up_place]
         place["name"] = name + "'s house"
         place["items"] = []
 
+        # Add in the actor to the state
         rand_state.actors[up_name] = deepcopy(ACTOR_TEMPLATE)
         actor = rand_state.actors[up_name]
         places = rand_state.places
@@ -45,6 +60,8 @@ def random_state():
         actor["grief"] = 0
 
 
+    # Once actors are added, initialize kill_desire and affection
+    # Each is a map from one actor to every other actor
     for source in rand_state.actors:
         for target in rand_state.actors:
             if source == target:
